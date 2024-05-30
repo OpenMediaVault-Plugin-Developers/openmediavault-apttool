78fc314e98a0a90cae38eeef14f066c7b540d946
Aaron Murray <plugins@omv-extras.org>
2024-05-30 12:08:29 -0500

    add dependency checking to prevent removing omv dependencies
    Signed-off-by: Aaron Murray <plugins@omv-extras.org>

---
+++ usr/share/openmediavault/confdb/migrations.d/conf.service.apttool_7.1.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# @license   http://www.gnu.org/licenses/gpl.html GPL Version 3
+# @author    Volker Theile <volker.theile@openmediavault.org>
+# @author    OpenMediaVault Plugin Developers <plugins@omv-extras.org>
+# @copyright Copyright (c) 2009-2013 Volker Theile
+# @copyright Copyright (c) 2013-2024 openmediavault plugin developers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+. /usr/share/openmediavault/scripts/helper-functions
+
+xpath="/config/services/apttool/packages/package"
+
+xmlstarlet sel -t -m "${xpath}" -v "uuid" -n ${OMV_CONFIG_FILE} |
+  xmlstarlet unesc |
+  while read uuid; do
+    if ! omv_config_exists "${xpath}[uuid='${uuid}']/dependency"; then
+      pn=$(omv_config_get "${xpath}[uuid='${uuid}']/packagename")
+      if [ $(apt-cache depends --recurse openmediavault | grep -Ec "^  Depends: ${pn}$") -eq 0 ]; then
+        depend=0
+      else
+        depend=1
+      fi
+      omv_config_add_key "${xpath}[uuid='${uuid}']" "dependency" ${depend}
+    fi
+  done;
+
+exit 0
